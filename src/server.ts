69a75a8f7a9863ffa02b0696146ac1c7062a0198
Pedro Rodrigues <peddroprp@gmail.com>
2021-09-29 09:00:38 -0300

    fga-eps-mds/2021.1-Pro-Especies-Docs#128 Add server to use PORT env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(4000, () => {
-  console.log(`server running on port 4000`);
+const serverPort = process.env.PORT || 4000;
+
+app.listen(serverPort, () => {
+  console.log('server running on port %d', serverPort);
 });
