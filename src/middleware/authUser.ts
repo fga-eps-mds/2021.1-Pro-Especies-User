ff460f53d66bc80101f052c81dc52e8401cc0dd8
PhRezende-eng <ph.hr.001@gmail.com>
2021-10-28 17:20:32 -0300

    #138-Remove expirin token

--- src/middleware/authUser.ts
+++ src/middleware/authUser.ts
@@ -2,8 +2,6 @@ import jwt from 'jsonwebtoken';
 
 export default class AuthUser {
   generateToken(data: object) {
-    return jwt.sign(data, process.env.AUTH_CONFIG_SECRET as string, {
-      expiresIn: '1d',
-    });
+    return jwt.sign(data, process.env.AUTH_CONFIG_SECRET as string, {});
   }
 }
